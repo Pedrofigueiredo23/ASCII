70bdc2611c996177202ec4c4c5cd3321277b39f9
Pedrofigueiredo23 <pedro_f23@hotmail.com>
2024-02-28 12:45:14 +0000

    2nd Commit Ponto Final

--- src/Main.java
+++ src/Main.java
@@ -22,19 +22,24 @@ public class Main {
 
     public static  StringBuilder transformacao(String input){ //método para transformar MAIUSCULAS e minusculas
         StringBuilder output = new StringBuilder();
-
+        boolean pontoFinal =true;
         for (int i = 0; i < input.length(); i++) {
             char currentChar = input.charAt(i);
 
-            if (currentChar >= 'A' && currentChar <= 'Z') {
+            if (currentChar >= 'A' && currentChar <= 'Z' && pontoFinal) {
                 // Converter maiúscula para minúscula
                 currentChar = (char) (currentChar + 32);
-            } else if (currentChar >= 'a' && currentChar <= 'z') {
+            } else if (currentChar >= 'a' && currentChar <= 'z' && pontoFinal) {
                 // Converter minúscula para maiúscula
                 currentChar = (char) (currentChar - 32);
             }
+            else if(currentChar == '.'){
+                pontoFinal= false;
+            }
             output.append(currentChar);
         }
+
+
         return output;
     }
 }
